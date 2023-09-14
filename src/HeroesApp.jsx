038749d06de5a6380c7f3ea8fdb1ce953a52ca4a
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2023-09-13 21:49:01 -0300

    Cambio de ruta imágenes

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,4 +1,5 @@
 import { AppRouter } from "./router/AppRouter"
+import 'animate.css';
 
 export const HeroesApp = () => {
   
--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -20,31 +20,32 @@ export const HeroCard = ({
     const heroImageUrl = `public/assets/heroes/${ id }.jpg`;
 
   return (
-    <div className="col">
+    <div className="col animate__animated animate__fadeIn">
         <div className="card">
 
             <div className="row no-gutters">
-                <img src={ heroImageUrl } alt={ superhero } className="card-img" />
-            </div>
+                <div className="col-4">
+                    <img src={ heroImageUrl } alt={ superhero } className="card-img" />
+                </div>
 
-            <div className="col-8">
-                <div className="card-body">
-                    <h5 className="card-title"> { superhero } </h5>
-                    <p className="card-text"> { alter_ego } </p>
+                <div className="col-8">
+                    <div className="card-body">
+                        <h5 className="card-title"> { superhero } </h5>
+                        <p className="card-text"> { alter_ego } </p>
 
-                    {/* Componente para mostrar los nombres de personajes */}
-                    <CharactersByHero alter_ego={ alter_ego } characters={ characters } />
+                        {/* Componente para mostrar los nombres de personajes */}
+                        <CharactersByHero alter_ego={ alter_ego } characters={ characters } />
 
-                    <p className="card-text">
-                        <small className="text-muted"> { first_appearance } </small>
-                    </p>
+                        <p className="card-text">
+                            <small className="text-muted"> { first_appearance } </small>
+                        </p>
 
-                    <Link to={ `/hero/${ id }` }>
-                        Más...
-                    </Link>
+                        <Link to={ `/hero/${ id }` }>
+                            Más...
+                        </Link>
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
   )
