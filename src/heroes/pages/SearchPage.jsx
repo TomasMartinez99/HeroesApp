9811675b4bfbaa80f92b67ec8266fea32fa4e606
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2023-09-14 16:35:50 -0300

    Buscador en construccion

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,52 @@
+import { useForm } from "../../hooks/useForm"
+import { HeroCard } from "../components"
 
 export const SearchPage = () => {
+
+  /* const { searchText } = useForm({
+    searchText: '';
+  }); */
+
   return (
-    <h1>SearchPage</h1>
+    <>
+      <h1>SearchPage</h1>
+      <hr />
+
+      <div className="col-5">
+        <h4>Searching</h4>
+        <hr />
+
+        <form>
+          <input 
+            type="text"
+            placeholder="Search a hero"
+            className="form-control"
+            name="searchText"
+            autoComplete="off"
+/*          value={ searchText }
+            onChange={ onInputChange } */
+          />
+
+          <button className="btn btn-outline-primary mt-1">
+            Search
+          </button>
+        </form>
+      </div>
+
+      <div className="col-7">
+        <h4>Results</h4>
+        <hr />
+
+        <div className="alert alert-primary">
+          Search a hero
+        </div>
+
+        <div className="alert alert-danger">
+          No hero with <b>ABC</b>
+        </div>
+
+        {/* <HeroCard /> */}
+      </div>
+    </>
   )
 }
