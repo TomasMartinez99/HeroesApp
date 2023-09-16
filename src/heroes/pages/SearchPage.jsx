4909e419cfcb227ff4ba5c2b78463a8e042d46e7
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2023-09-16 16:59:11 -0300

    Buscador en funcionamiento

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,51 +1,74 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import { useForm } from "../../hooks/useForm"
+import queryString from "query-string";
+import { getHeroesByName } from "../helpers/getHeroesByName";
 import { HeroCard } from "../components"
 
 export const SearchPage = () => {
 
-  /* const { searchText } = useForm({
-    searchText: '';
-  }); */
+  const navigate = useNavigate();
+  // Para obtener la ubicacion de donde nos encontramos
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse( location.search );
+  const heroes = getHeroesByName(q);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if ( searchText.trim().length <= 1 ) return;
+    
+    navigate( `?q=${ searchText }` );
+  }
 
   return (
     <>
       <h1>SearchPage</h1>
       <hr />
 
-      <div className="col-5">
-        <h4>Searching</h4>
-        <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
 
-        <form>
-          <input 
-            type="text"
-            placeholder="Search a hero"
-            className="form-control"
-            name="searchText"
-            autoComplete="off"
-/*          value={ searchText }
-            onChange={ onInputChange } */
-          />
+          <form onSubmit={ onSearchSubmit }>
+            <input 
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={ searchText }
+              onChange={ onInputChange }
+            />
 
-          <button className="btn btn-outline-primary mt-1">
-            Search
-          </button>
-        </form>
-      </div>
-
-      <div className="col-7">
-        <h4>Results</h4>
-        <hr />
-
-        <div className="alert alert-primary">
-          Search a hero
+            <button className="btn btn-outline-primary mt-1">
+              Search
+            </button>
+          </form>
         </div>
 
-        <div className="alert alert-danger">
-          No hero with <b>ABC</b>
-        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
 
-        {/* <HeroCard /> */}
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            No hero with <b>{ q }</b>
+          </div>
+
+          {
+            heroes.map( hero => (
+              <HeroCard key={ hero.id } { ...hero }/>
+            ))
+          }
+        </div>
       </div>
     </>
   )
