ed1598c9293c2a2760c46b5fd4f5f1630e4d9e62
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2023-09-18 16:42:15 -0300

    Alertas de buscador en funcionamiento

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -19,7 +19,7 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if ( searchText.trim().length <= 1 ) return;
+    /* if ( searchText.trim().length <= 1 ) return; */
     
     navigate( `?q=${ searchText }` );
   }
@@ -55,14 +55,13 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
-            Search a hero
-          </div>
-
-          <div className="alert alert-danger">
-            No hero with <b>{ q }</b>
-          </div>
-
+          {/* Condición para mostrar alertas */}
+          {
+            ( q === '' ) 
+              ?  <div className="alert alert-primary"> Search a hero </div>
+              :  ( heroes.length === 0 ) && <div className="alert alert-danger"> No hero with <b>{ q }</b> </div>
+          }
+          
           {
             heroes.map( hero => (
               <HeroCard key={ hero.id } { ...hero }/>
